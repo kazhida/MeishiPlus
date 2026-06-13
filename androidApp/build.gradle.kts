f8d3dcd7e627d4e219bd47bbf85d500dfbbfa05f
kazhida <kazhida@gmail.com>
2026-06-13 22:00:45 +0900

    バージョンコードの更新

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         applicationId = "com.abplus.meishiplus"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
     }
     packaging {
